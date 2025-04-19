65a94cd74d7b60754850862f9626645069b41c2a
SecrizMRF <secrizkyfadhillah@gmail.com>
2025-04-19 11:09:40 +0700

    JAWABAN_241401055_UTS-AP1

---
+++ UTS AP 1/soal1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    system("cls");
+
+    float kode;
+    int i = 1;
+
+    cout << "Kode : ";
+    cin >> kode;
+
+    while (i < kode) {
+        i *= 2;
+    } if (i == kode) {
+        cout << "Ya ya ya :3" << endl;
+    } else {
+        cout << "Nooo!! Bukan :(" << endl;
+    }
+
+    char ulang;
+    cout << "\nKamoeh mawu mengulang program? (Y/N)"<<endl;
+    cin >> ulang;
+    getchar();
+    if (ulang == 'Y' || ulang == 'y') { 
+        main();
+    } else {
+        cout << "\n//==//==// Program selesai //==//==//" << endl;
+    }
+}
